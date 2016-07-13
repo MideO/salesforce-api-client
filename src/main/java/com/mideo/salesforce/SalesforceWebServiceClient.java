5871ea236ab6ea2dccfeb6df98ae530bbaf4bb7e
Mide Ojikutu <olamide.ojikutu@bskyb.com>
2016-07-13 19:44:46 +0100

    reformate client

--- src/main/java/com/mideo/salesforce/SalesforceWebServiceClient.java
+++ src/main/java/com/mideo/salesforce/SalesforceWebServiceClient.java
@@ -22,30 +22,16 @@ public class SalesforceWebServiceClient {
     private Batch batch;
     private DataFetcher dataFetcher;
     private SObjectApi SObjectApi;
-
-
-
     private long publishStatusCheckTimeout = 10000;
 
     /**
-     *
-     * @param publishStatusCheckTimeoutInMilliSeconds - Polling timeout in milliseconds
-     * @return SalesforceWebServiceClient
-     */
-    public SalesforceWebServiceClient setPublishStatusCheckTimeout(long publishStatusCheckTimeoutInMilliSeconds) {
-        this.publishStatusCheckTimeout = publishStatusCheckTimeoutInMilliSeconds;
-        return this;
-    }
-
-    /**
-     *  @param salesforceConnectionClient Initiated Salesforce Connection client
-     *
-     *  <br >Usage:<br >
-     *     SalesforceConfig config = new SalesforceConfig("abc").clientId("wewew").clientSecret("dfdfd").userName("sdsds").password("sdsds").userToken("sdssd");<br >
-     *     HttpRequestSpecificationBuilder httpRequestSpecificationBuilder = new HttpRequestSpecificationBuilder();<br >
-     *     SalesforceConnectionClient connectionClient = new SalesforceConnectionClient(config, httpRequestSpecificationBuilder);<br >
-     *     SalesforceWebServiceClient webClient = new SalesforceWebServiceClient(connectionClient);<br >
-     *
+     * @param salesforceConnectionClient Initiated Salesforce Connection client
+     *                                   <p>
+     *                                   <br >Usage:<br >
+     *                                   SalesforceConfig config = new SalesforceConfig("abc").clientId("wewew").clientSecret("dfdfd").userName("sdsds").password("sdsds").userToken("sdssd");<br >
+     *                                   HttpRequestSpecificationBuilder httpRequestSpecificationBuilder = new HttpRequestSpecificationBuilder();<br >
+     *                                   SalesforceConnectionClient connectionClient = new SalesforceConnectionClient(config, httpRequestSpecificationBuilder);<br >
+     *                                   SalesforceWebServiceClient webClient = new SalesforceWebServiceClient(connectionClient);<br >
      */
     public SalesforceWebServiceClient(SalesforceConnectionClient salesforceConnectionClient) {
         this.salesforceConnectionClient = salesforceConnectionClient;
@@ -56,12 +42,9 @@ public class SalesforceWebServiceClient {
     }
 
     /**
-     *
-     * @param csvInputStream RFC4180 [double qouted CSV, comma seperated, row ended with `\r\n`] Input String from String or file
+     * @param csvInputStream   RFC4180 [double qouted CSV, comma seperated, row ended with `\r\n`] Input String from String or file
      * @param targetObjectName Salesforce Object Name
-     *
      * @return PublishResult publish result object
-     *
      * @throws AsyncApiException Saleforce Api AsyncApiException
      **/
     public PublishResult publishCsvToTable(InputStream csvInputStream, String targetObjectName) throws AsyncApiException {
@@ -79,12 +62,18 @@ public class SalesforceWebServiceClient {
     }
 
     /**
-     *
-     * @param jobId Salesoforce Job Id
+     * @param publishStatusCheckTimeoutInMilliSeconds - Polling timeout in milliseconds
+     * @return SalesforceWebServiceClient
+     */
+    public SalesforceWebServiceClient setPublishStatusCheckTimeout(long publishStatusCheckTimeoutInMilliSeconds) {
+        this.publishStatusCheckTimeout = publishStatusCheckTimeoutInMilliSeconds;
+        return this;
+    }
+
+    /**
+     * @param jobId   Salesoforce Job Id
      * @param batchId Saleforce Batch Id
-     *
      * @return String value of Status of Published Batch
-     *
      * @throws AsyncApiException Saleforce Api AsyncApiException
      **/
     public String getPublishedDataStatus(String jobId, String batchId) throws AsyncApiException {
@@ -95,14 +84,57 @@ public class SalesforceWebServiceClient {
 
 
     /**
-     *
+     * @param sObjectName Salesforce Object Name
+     * @param data        key value pair for each data column to be populated
+     * @return String value of created Object Id
+     * @throws ConnectionException <br >Usage:<br >
+     *                             Map&lt;String, Object&gt; accountDataMap = new HashMap&lt;String, String&gt;();<br>
+     *                             accountDataMap.put("Name", "testName Surname");<br>
+     *                             accountDataMap.put("email", "testName@example.com");<br><br>
+     *                             <p>
+     *                             webClient.createObject("Account", accountDataMap)<br>
+     **/
+    public String createObject(String sObjectName, Map<String, Object> data) throws ConnectionException {
+        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
+                .createSObject(sObjectName, data);
+    }
+
+    /**
+     * @param sObjectName Salesforce Object Name
+     * @param data        key value pair for each data column to be populated
+     * @param id          Salesforce Object Id
+     * @return String value of created Object Id
+     * @throws ConnectionException <br >Usage:<br >
+     *                             Map&lt;String, Object&gt; accountDataMap = new HashMap&lt;String, String&gt;();<br>
+     *                             accountDataMap.put("Name", "testName Surname");<br>
+     *                             accountDataMap.put("email", "testName@example.com");<br>
+     *                             webClient.updateObject("Account", accountDataMap)<br>
+     **/
+    public String updateObject(String sObjectName, String id, Map<String, Object> data) throws ConnectionException {
+        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
+                .updateSObject(sObjectName, id, data);
+    }
+
+
+    /**
+     * @param sObjectName Salesforce Object Name
+     * @param id          sObject salesforce Id
+     * @return Map of Object Fields and values
+     * @throws ConnectionException <br >Usage: <br >
+     *                             webClient.retrieveSObject("Account", OjectId)
+     **/
+    public Map<String, Object> retrieve(String sObjectName, String id) throws ConnectionException {
+        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
+                .retrieveSObject(sObjectName, id);
+    }
+
+
+    /**
      * @param targetObjectName Salesforce Object Name
-     *
      * @return List of Maps representing each data row
-     *
-     * @throws AsyncApiException Saleforce Api AsyncApiException
+     * @throws AsyncApiException   Saleforce Api AsyncApiException
      * @throws ConnectionException Saleforce Api ConnectionException
-     * @throws IOException Java IOException
+     * @throws IOException         Java IOException
      **/
     public List<Map<String, String>> exportDataFromTable(String targetObjectName) throws Exception {
         List<String> columns = SObjectApi.withSalesforceClient(salesforceConnectionClient)
@@ -112,35 +144,29 @@ public class SalesforceWebServiceClient {
     }
 
     /**
-     *
      * @param targetObjectName Salesforce Object Name
-     * @param columns Columns to retrieve
-     *
+     * @param columns          Columns to retrieve
      * @return List of Maps representing each data row
-     *
-     * @throws AsyncApiException Saleforce Api AsyncApiException
+     * @throws AsyncApiException   Saleforce Api AsyncApiException
      * @throws ConnectionException Saleforce Api ConnectionException
-     * @throws IOException Java IOException
-     *
-     *   <br >Usage:<br >
-     *     List&lt;String&gt; columns = new ArrayList();<br>
-     *     columns.add("Id");<br>
-     *     webClient.exportDataFromTable("Account", columns);<br >
+     * @throws IOException         Java IOException
+     *                             <p>
+     *                             <br >Usage:<br >
+     *                             List&lt;String&gt; columns = new ArrayList();<br>
+     *                             columns.add("Id");<br>
+     *                             webClient.exportDataFromTable("Account", columns);<br >
      **/
     public List<Map<String, String>> exportDataFromTable(String targetObjectName, List<String> columns) throws Exception {
         return exportDataFromTable(targetObjectName, columns, new HashMap<String, String>());
     }
 
     /**
-     *
      * @param targetObjectName Salesforce Object Name
-     * @param filters - Map of filters
-     *
+     * @param filters          - Map of filters
      * @return List of Maps representing each data row
-     *
-     * @throws AsyncApiException Saleforce Api AsyncApiException
+     * @throws AsyncApiException   Saleforce Api AsyncApiException
      * @throws ConnectionException Saleforce Api ConnectionException
-     * @throws IOException Java IOException
+     * @throws IOException         Java IOException
      **/
     public List<Map<String, String>> exportDataFromTable(String targetObjectName, Map<String, String> filters) throws Exception {
         List<String> columns = SObjectApi.withSalesforceClient(salesforceConnectionClient)
@@ -150,35 +176,32 @@ public class SalesforceWebServiceClient {
     }
 
     /**
-     *
      * @param targetObjectName Salesforce Object Name
-     * @param columns Columns to retrieve
-     * @param filters - Map of filters
+     * @param columns          Columns to retrieve
+     * @param filters          - Map of filters
      * @return List of Maps representing each data row
-     *
-     * @throws AsyncApiException Saleforce Api AsyncApiException
+     * @throws AsyncApiException   Saleforce Api AsyncApiException
      * @throws ConnectionException Saleforce Api ConnectionException
-     * @throws IOException Java IOException
-     *
-     *   <br >Usage:<br >
-     *     List&lt;String&gt; columns = new ArrayList();<br>
-     *     columns.add("Id");<br>
-     *     Map&lt;String,String&gt; filters = new HashMap&lt;String,String&gt;();<br>
-     *     filters.put("id", "abc123");<br>
-     *     webClient.exportDataFromTable("Account", columns);<br >
+     * @throws IOException         Java IOException
+     *                             <p>
+     *                             <br >Usage:<br >
+     *                             List&lt;String&gt; columns = new ArrayList();<br>
+     *                             columns.add("Id");<br>
+     *                             Map&lt;String,String&gt; filters = new HashMap&lt;String,String&gt;();<br>
+     *                             filters.put("id", "abc123");<br>
+     *                             webClient.exportDataFromTable("Account", columns);<br >
      **/
-    public List<Map<String, String>> exportDataFromTable(String targetObjectName, List<String> columns, Map<String, String> filters) throws Exception{
-        String QUERY_TEMPLATE = "SELECT %s FROM "+targetObjectName;
+    public List<Map<String, String>> exportDataFromTable(String targetObjectName, List<String> columns, Map<String, String> filters) throws Exception {
+        String QUERY_TEMPLATE = "SELECT %s FROM " + targetObjectName;
         List<String> filterList = new ArrayList<>();
 
-        for (Map.Entry entry: filters.entrySet()) {
+        for (Map.Entry entry : filters.entrySet()) {
             filterList.add(String.format("%s='%s'", entry.getKey(), entry.getValue()));
         }
-        QUERY_TEMPLATE += filterList.size() == 0? "": " WHERE "+StringUtils.join(filterList, ',');
+        QUERY_TEMPLATE += filterList.size() == 0 ? "" : " WHERE " + StringUtils.join(filterList, ',');
 
-        String query = String.format(QUERY_TEMPLATE, StringUtils.join(columns,','));
+        String query = String.format(QUERY_TEMPLATE, StringUtils.join(columns, ','));
         ByteArrayInputStream soqlInputStream = new ByteArrayInputStream(query.getBytes());
-
 
 
         JobInfo jobInfo = job.withSalesforceClient(salesforceConnectionClient)
@@ -197,83 +220,20 @@ public class SalesforceWebServiceClient {
         long counter = 0;
         long sleeptime = 1000;
 
-        while(counter <= publishStatusCheckTimeout){
-            if (getPublishedDataStatus(jobInfo.getId(), batchInfo.getId()).equals(BatchStateEnum.Completed.name())){
+        while (counter <= publishStatusCheckTimeout) {
+            if (getPublishedDataStatus(jobInfo.getId(), batchInfo.getId()).equals(BatchStateEnum.Completed.name())) {
                 return dataFetcher.withSalesforceClient(salesforceConnectionClient)
                         .fetchData(jobInfo.getId(), batchInfo.getId());
             }
-            if (getPublishedDataStatus(jobInfo.getId(), batchInfo.getId()).equals(BatchStateEnum.Failed.name())){
+            if (getPublishedDataStatus(jobInfo.getId(), batchInfo.getId()).equals(BatchStateEnum.Failed.name())) {
 
                 throw new FailedBulkOperationException("Salesforce Bulk Api Operation Failed: \n" + batch.batchInfo);
             }
             Thread.sleep(sleeptime);
-            counter+=sleeptime;
+            counter += sleeptime;
 
         }
 
         return null;
     }
-
-
-
-    /**
-     *
-     * @param sObjectName Salesforce Object Name
-     * @param data key value pair for each data column to be populated
-     *
-     * @return String value of created Object Id
-     *
-     * @throws ConnectionException
-     *
-     *   <br >Usage:<br >
-     *     Map&lt;String, Object&gt; accountDataMap = new HashMap&lt;String, String&gt;();<br>
-     *     accountDataMap.put("Name", "testName Surname");<br>
-     *     accountDataMap.put("email", "testName@example.com");<br><br>
-     *
-     *     webClient.createObject("Account", accountDataMap)<br>
-     **/
-    public String createObject(String sObjectName, Map<String, Object> data) throws ConnectionException {
-        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
-                .createSObject(sObjectName, data);
-    }
-
-    /**
-     *
-     * @param sObjectName Salesforce Object Name
-     * @param data key value pair for each data column to be populated
-     * @param id Salesforce Object Id
-     *
-     * @return String value of created Object Id
-     *
-     * @throws ConnectionException
-     *
-     *   <br >Usage:<br >
-     *     Map&lt;String, Object&gt; accountDataMap = new HashMap&lt;String, String&gt;();<br>
-     *     accountDataMap.put("Name", "testName Surname");<br>
-     *     accountDataMap.put("email", "testName@example.com");<br>
-     *     webClient.updateObject("Account", accountDataMap)<br>
-     **/
-    public String updateObject(String sObjectName, String id, Map<String, Object> data) throws ConnectionException {
-        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
-                .updateSObject(sObjectName, id, data);
-    }
-
-
-    /**
-     *
-     * @param sObjectName Salesforce Object Name
-     * @param id sObject salesforce Id
-     *
-     * @return Map of Object Fields and values
-     *
-     * @throws ConnectionException
-     *
-     *   <br >Usage: <br >
-     *     webClient.retrieveSObject("Account", OjectId)
-     **/
-    public Map<String, Object> retrieve(String sObjectName, String id) throws ConnectionException {
-        return SObjectApi.withSalesforceClient(salesforceConnectionClient)
-                .retrieveSObject(sObjectName, id);
-    }
-
 }
